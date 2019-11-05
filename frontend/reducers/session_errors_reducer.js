1fd330df47130e7a47cfdb3cd9a3d0d71793b0d7
alfredosumosav <asumosa1993@gmail.com>
2019-11-05 15:35:41 -0800

    Creates session errors reducer

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,23 @@
+import {
+    RECEIVE_CURRENT_USER,
+    RECEIVE_SESSION_ERRORS
+} from "../actions/session_actions";
+
+const _nullErrors = [];
+
+const sessionErrorsReducer = (oldState = _nullErrors, action) => {
+    Object.freeze(oldState);
+    let nextState = Object.assign({}, oldState);
+
+    switch (action.type) {
+        case RECEIVE_SESSION_ERRORS:
+            return Object.values(action.errors);
+        case RECEIVE_CURRENT_USER:
+            return _nullErrors;
+
+        default:
+            return oldState;
+    }
+};
+
+export default sessionErrorsReducer;
