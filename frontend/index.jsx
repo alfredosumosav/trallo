62b9230450ae383feb606f99a81b715e09b5b284
alfredosumosav <asumosa1993@gmail.com>
2019-11-05 15:40:32 -0800

    Add changes to the entry file

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import {
+    signup,
+    logout,
+    login
+} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     let preloadedState = undefined;
+    const store = configureStore(preloadedState);
+
+    // TESTING
+    window.user = {
+        username: "alfredo",
+        password: "alfredo"
+    };
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.login = login;
+    window.logout = logout;
+    // TESTING END
 
     // if (window.currentUser) {
     //     preloadedState = {
@@ -14,7 +31,5 @@ document.addEventListener('DOMContentLoaded', () => {
     //         }
     //     };
     // }
-    
-    const store = configureStore(preloadedState);
     ReactDOM.render(<Root store={store} />, root);
 })
