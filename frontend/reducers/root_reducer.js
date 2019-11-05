b89eae4379b62cce2c220a289949e995e75387fa
alfredosumosav <asumosa1993@gmail.com>
2019-11-05 15:37:29 -0800

    Combine more reducers to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 // import entitiesReducer from './entities';
-import sessionReducer from './session';
+import entitiesReducer from './entities_reducer';
+import sessionReducer from './session_reducer';
+import errorsReducer from './errors_reducer';
 
-const RootReducer = combineReducers({
-    // entities: entitiesReducer,
-    session: sessionReducer
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+    errors: errorsReducer
 });
 
-export default RootReducer;
+export default rootReducer;
