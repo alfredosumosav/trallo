4eb0fe89596186910d775dce3608a8c4c90ede82
alfredosumosav <asumosa1993@gmail.com>
2019-11-04 20:51:42 -0800

    Creates SessionReducer and RootReducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import entitiesReducer from './entities';
+import sessionReducer from './session';
+
+const RootReducer = combineReducers({
+    // entities: entitiesReducer,
+    session: sessionReducer
+});
+
+export default RootReducer;
