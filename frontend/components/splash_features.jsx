4226cce25f571e01a07ea628a6496755b85c2dd1
alfredosumosav <asumosa1993@gmail.com>
2019-11-07 18:33:25 -0800

    Creates splash features component

--- frontend/components/splash_features.jsx
+++ frontend/components/splash_features.jsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SplashCollaborate = (props) => {
+const SplashFeatures = (props) => {
     return (
-        <div className="splash-container collaborate-container">
-            <div className="splash-row">
-                <div className="splash-column sm-col">
-                    <h3>Work with any team</h3>
-                    <p>Whether it’s for work, a side project or even the next family vacation, Trello helps your team stay organized.</p>
-                    <p><Link className="btn btn-secondary" to="/signup">Start&nbsp;doing&nbsp;→</Link></p>
+        <div id="features" className="splash-container features-container">
+            <div className="splash-col">
+                <div className="splash-row">
+                    <div id="f1" className="splash-col text-center">
+                        <h3>Trallo your way</h3>
+                        <p>Use Trello the way your team works best. We’ve got the flexibility & features to fit any team’s style.</p>
+                    </div>
                 </div>
-                <div className="splash-column">
-                    <div className="board-img">
-                        <img src="https://trello-attachments.s3.amazonaws.com/5b9270821913198b25dc6c3f/5da88172fa474e353d3190de/6fef214f283f536655eb0584b895e9df/board.png" alt="board" />
+                <div className="splash-row">
+                    <div className="splash-col splash-card">
+                        <div className="img">
+                            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/0cad30a99820b0d840a5b48635d00b6e/updated-layouts-collab.png" width="270" alt=""></img>
+                        </div>
+                        <h5>The Team Playbook</h5>
+                        <p className="small">
+                            It’s easy to get your team up and running with Trello. We’ve collected all of the boards and tools you need to succeed in one handy resource.
+                        </p>
+                        <p>
+                            <Link className="btn btn-secondary" to="/signup">Make&nbsp;A&nbsp;Game&nbsp;Plan</Link>
+                        </p>
+                    </div>
+                    <div className="splash-col splash-card">
+                        <div className="img">
+                            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/15e77c8347135120a438d17e38195e44/updated-layouts-platform.png" width="270" alt=""></img>
+                        </div>
+                        <h5>A Productivity Plataform</h5>
+                        <p className="small">
+                            Integrate the apps your team already uses directly into your workflow. Power-Ups turn Trello boards into living applications to meet your team's unique business needs.
+                        </p>
+                        <p>
+                            <Link className="btn btn-secondary" to="/signup">Power-Up&nbsp;Your&nbsp;Workflow</Link>
+                        </p>
+                    </div>
+                    <div className="splash-col splash-card">
+                        <div className="img">
+                            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/6eaaf5e37ab67a8ed6cd7764660513b9/updated-layouts-sync.png" width="270" alt=""></img>
+                        </div>
+                        <h5>Always In Sync</h5>
+                        <p className="small">
+                            No matter where you are, Trello stays in sync across all of your devices. Collaborate with your team anywhere, from sitting on the bus to sitting on the beach.
+                        </p>
+                        <p>
+                            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/eebf313cb223112b503b7322173b013c/btn-appstore-black.png" className="img-fluid" alt="Available on the App Store"></img>
+                            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/bcde9868a665af4ab4efe460c46fbc7b/btn-playstore-black.png" className="img-fluid" alt="Android App on Google Play"></img>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -20,4 +55,4 @@ const SplashCollaborate = (props) => {
     );
 }
 
-export default SplashCollaborate;
+export default SplashFeatures;
