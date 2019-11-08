147594edcf959e0d0cf02402544b99e8614e4e0a
alfredosumosav <asumosa1993@gmail.com>
2019-11-07 21:21:16 -0800

    Removes comments

--- frontend/components/splash_get_started.jsx
+++ frontend/components/splash_get_started.jsx
@@ -9,13 +9,6 @@ const SplashGetStarted = (props) => {
                 <p>Sign up and become one of the millions of people around the world using Trello to get more done.</p>
                 <p><Link className="btn btn-success" to="/signup">Get&nbsp;Started&nbsp;-&nbsp;It's&nbsp;Free!</Link></p>
             </div>
-                {/* <div className="splash-column">
-                    <div className="splash-row">
-                        <h3>Work with any team</h3>
-                        <p>Whether it’s for work, a side project or even the next family vacation, Trello helps your team stay organized.</p>
-                        <p><Link className="btn btn-secondary" to="/signup">Start&nbsp;doing&nbsp;→</Link></p>
-                    </div>
-                </div> */}
         </div>
     );
 }
