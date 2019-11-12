607ca104ccc9986e42e4838e63f6810265971308
alfredosumosav <asumosa1993@gmail.com>
2019-11-12 10:30:07 -0800

    Adds modal to main page and redirect users to /dashboard when logged in

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import GreetingContainer from './greeting_container';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
+import BoardShowContainer from './boards/board_show_container';
+import Modal from './modal/modal';
 import NavBarContainer from './nav_bar_container';
 import SplashContainer from './splash_container';
 import BoardIndexContainer from './boards/board_index_container';
 import Footer from './footer';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
 import {
     AuthRoute,
     AuthRoute2,
@@ -22,23 +24,26 @@ class App extends React.Component {
     render() {
         return (
             <div className="main">
-                    <header id="header">
-                        <nav className="navbar">
-                            <div id="logo-container">
-                                <i className="fab fa-trello"></i>
-                                <Link id="logo" to={"/"}>Trallo</Link>
-                            </div>
-                            <Route exact to="/" component={NavBarContainer} />
-                        </nav>
-                    </header>
-                    <Switch>
-                        <AuthRoute path="/login" component={LoginFormContainer} />
-                        <AuthRoute path="/signup" component={SignupFormContainer} />
-                        <ProtectedRoute path="/dashboard" component={BoardIndexContainer} />
-                        <AuthRoute2 path="/" component={SplashContainer} />
-                        {/* <Route to="/" component={SplashContainer} /> */}
-                    </Switch>
-                    <Footer />
+                <Modal />
+                <header id="header">
+                    <nav className="navbar">
+                        <div id="logo-container">
+                            <i className="fab fa-trello"></i>
+                            <Link id="logo" to={"/"}>Trallo</Link>
+                        </div>
+                        <Route exact to="/" component={NavBarContainer} />
+                    </nav>
+                </header>
+                <Switch>
+                    <AuthRoute path="/login" component={LoginFormContainer} />
+                    <AuthRoute path="/signup" component={SignupFormContainer} />
+                    <ProtectedRoute path="/dashboard" component={BoardIndexContainer} />
+                    <Route exact path="/boards/:boardId" component={BoardShowContainer} />
+                    <AuthRoute2 exact path="/" component={SplashContainer} />
+                    <Redirect to="/" />
+                    {/* <Route to="/" component={SplashContainer} /> */}
+                </Switch>
+                <Footer />
             </div>
         );
     }
