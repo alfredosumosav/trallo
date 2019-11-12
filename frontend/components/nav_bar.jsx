f5f9fd90a6f0fde36e58e72d06e43e1308dbf187
alfredosumosav <asumosa1993@gmail.com>
2019-11-12 10:32:16 -0800

    Adds button to create a new board using the modal component

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({ currentUser, logout }) => {
+const NavBar = ({ currentUser, logout, openModal }) => {
     const display = currentUser ? (
         <div id="welcome">
             <h2 className="header-name">Hi, {currentUser.username}!</h2>
             <div className="btn-container"><button className="header-button btn btn-logout" onClick={logout}>Log Out</button></div>
+            <div className="plus btn">
+                <button className="btn" onClick={() => openModal('create_board')}>+</button>
+            </div>
         </div>
     ) : (
             <div id="session-actions-container">
