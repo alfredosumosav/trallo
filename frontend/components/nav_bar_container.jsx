370277e5374339c2c250ef575b28ecc1d69dbb32
alfredosumosav <asumosa1993@gmail.com>
2019-11-12 10:31:28 -0800

    Adds openmodal prop to navbar

--- frontend/components/nav_bar_container.jsx
+++ frontend/components/nav_bar_container.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import NavBar from './nav_bar';
 
 import { logout } from '../actions/session_actions';
+import { openModal } from '../actions/modal_actions';
 
 const mapStateToProps = state => ({
     currentUser: state.entities.users[state.session.id]
@@ -10,6 +11,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     logout: () => dispatch(logout()),
+    openModal: modal => dispatch(openModal(modal))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
