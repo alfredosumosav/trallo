0de04e93c051ea10450c0f3787b03c870d87067a
alfredosumosav <asumosa1993@gmail.com>
2019-12-11 13:53:02 -0800

    Create migration and add fields on database

---
+++ db/migrate/20191211214828_create_lists.rb
@@ -0,0 +1,14 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.string :title, null: false
+      t.integer :board_id, null: false
+      t.integer :author_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :lists, :board_id
+    add_index :lists, :author_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_002414) do
+ActiveRecord::Schema.define(version: 2019_12_11_214828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 2019_12_11_002414) do
     t.boolean "archived", default: false, null: false
     t.index ["author_id"], name: "index_boards_on_author_id"
     t.index ["title"], name: "index_boards_on_title"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "board_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_lists_on_author_id"
+    t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
   create_table "users", force: :cascade do |t|
