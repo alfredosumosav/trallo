f93f07d6e7661670fde5a8fb24f987beadf872e3
alfredosumosav <asumosa1993@gmail.com>
2019-12-11 13:55:24 -0800

    Add validations to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: lists
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  board_id   :integer          not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class List < ApplicationRecord
+    validates :title, :board_id, :author_id, presence: true
+    
 end
