096cfeae0b9aa023c6c3b4656445852bccc40498
alfredosumosav <asumosa1993@gmail.com>
2019-12-11 15:20:32 -0800

    Create list api utils

---
+++ frontend/utils/list_api_utils.js
@@ -0,0 +1,40 @@
+export const fetchLists = () => {
+    return $.ajax({
+        url: `/api/lists`,
+        method: 'GET'
+    })
+};
+
+export const fetchList = listId => {
+    return $.ajax({
+        url: `/api/lists/${listId}`,
+        method: 'GET'
+    })
+};
+
+export const createList = list => {
+    return $.ajax({
+        url: `/api/lists`,
+        method: 'POST',
+        data: list,
+        contentType: false,
+        proccessData: false
+    })
+};
+
+export const updateList = list => {
+    return $.ajax({
+        url: `/api/lists/${list.id}`,
+        method: 'PATCH',
+        data: list,
+        contentType: false,
+        proccessData: false
+    })
+};
+
+export const deleteList = listId => {
+    return $.ajax({
+        url: `/api/lists/${listId}`,
+        method: 'DELETE'
+    })
+};
