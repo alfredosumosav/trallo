24c6a6fb99ed808a900d7d241e0becfc2983c8fa
alfredosumosav <asumosa1993@gmail.com>
2019-12-11 15:20:39 -0800

    Create lists reducer

---
+++ frontend/reducers/lists_reducer.js
@@ -0,0 +1,26 @@
+import {
+    RECEIVE_LISTS,
+    RECEIVE_LIST,
+    REMOVE_LIST
+} from '../actions/list_actions';
+
+const listsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    let nextState = Object.assign({}, oldState);
+
+    switch (action.type) {
+        case RECEIVE_LISTS:
+            return action.lists;
+        case RECEIVE_LIST:
+            // let list = Object.values(action.list)[0]
+            // nextState[list.id] = list;
+            return nextState;
+        case REMOVE_LIST:
+            // delete nextState[action.listId];
+            return nextState;
+        default:
+            return oldState;
+    }
+};
+
+export default listsReducer;
