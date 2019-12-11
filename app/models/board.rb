444fcbe025ca72863be492b286e5ba2a2b72e96b
alfredosumosav <asumosa1993@gmail.com>
2019-12-11 13:55:31 -0800

    Annotate models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  favorited  :boolean          default("false"), not null
+#  archived   :boolean          default("false"), not null
+#
+
 class Board < ApplicationRecord
     validates :title, :author_id, presence: true
     
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  favorited  :boolean          default("false"), not null
+#  archived   :boolean          default("false"), not null
+#
+
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
