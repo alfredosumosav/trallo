af65491addca4b93b4acdf25d44f4bb51dadf77c
alfredosumosav <asumosa1993@gmail.com>
2019-12-11 13:59:56 -0800

    Add lists associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -12,5 +12,14 @@
 
 class List < ApplicationRecord
     validates :title, :board_id, :author_id, presence: true
-    
+
+    belongs_to :board,
+        primary_key: :id,
+        foreign_key: :board_id,
+        classname: :Board
+
+    belongs_to :author,
+        primary_key: :id,
+        foreign_key: :author_id,
+        classname: :User
 end
