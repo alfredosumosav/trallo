f825bdd0235844a001a9545cf709bfac406dabfd
alfredosumosav <asumosa1993@gmail.com>
2019-12-16 16:55:49 -0800

    Add create list prop to show container

--- frontend/components/boards/board_show_container.jsx
+++ frontend/components/boards/board_show_container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import BoardShow from './board_show';
 import { requestBoard, updateBoard, deleteBoard } from '../../actions/board_actions';
-import { requestLists, requestList, updateList, deleteList } from '../../actions/list_actions';
+import { requestLists, createList, requestList, updateList, deleteList } from '../../actions/list_actions';
 
 class BoardShowEdit extends React.Component {
     componentDidMount() {
@@ -13,12 +13,12 @@ class BoardShowEdit extends React.Component {
 
     render() {
         // debugger
-        if (this.props.board === undefined) {
+        if (this.props.board === undefined || this.props.lists === undefined) {
             return null;
         }
 
         return (
-            <BoardShow lists={this.props.lists} board={this.props.board} updateBoard={this.props.updateBoard} deleteBoard={this.props.deleteBoard} />
+            <BoardShow lists={this.props.lists} board={this.props.board} updateBoard={this.props.updateBoard} deleteBoard={this.props.deleteBoard} createList={this.props.createList} />
         )
     }
 }
@@ -34,7 +34,8 @@ const mDTP = (dispatch, ownProps) => ({
     requestBoard: boardId => dispatch(requestBoard(boardId)),
     updateBoard: board => dispatch(updateBoard(board)),
     deleteBoard: boardId => dispatch(deleteBoard(boardId)),
-    requestLists: boardId => dispatch(requestLists(boardId))
+    requestLists: boardId => dispatch(requestLists(boardId)),
+    createList: list => dispatch(createList(list))
 });
 
 export default connect(mSTP, mDTP)(BoardShowEdit);
