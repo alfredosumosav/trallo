a6effc35c8506093b0ad0fc9548ec15fcd977782
alfredosumosav <asumosa1993@gmail.com>
2019-12-19 21:21:53 -0800

    Update social presence

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -6,8 +6,9 @@ const Footer = (props) => {
         <div className="footer-container">
             <div className="social">
                 <a href="https://github.com/alfredosumosav" target="_blank"><i className="fab fa-github icon"></i></a>
-                <a href="https://www.facebook.com/alfredo.d.sumosa" target="_blank"><i className="fab fa-facebook-square icon"></i></a>
-                <a href="https://www.instagram.com/asumosa93/" target="_blank"><i className="fab fa-instagram icon"></i></a>
+                <a href="https://angel.co/alfredo-david-sumosa" target="_blank"><i className="fab fa-angellist icon"></i></a>
+                <a href="https://www.linkedin.com/in/asumosa" target="_blank"><i className="fab fa-linkedin icon"></i></a>
+                <a href="mailto:asumosa1993@gmail.com" target="_blank"><i className="far fa-envelope icon"></i></a>
             </div>
             <div className="note">
                 <p>&nbsp;©&nbsp;Copyright&nbsp;{year}.&nbsp;All&nbsp;rights&nbsp;reserved.</p>
