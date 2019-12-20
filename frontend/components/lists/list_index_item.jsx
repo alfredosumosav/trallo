9bb522b31643b8acd0a4335683100b1a16cf6557
alfredosumosav <asumosa1993@gmail.com>
2019-12-19 21:05:44 -0800

    Utilized update list prop and handle form data

--- frontend/components/lists/list_index_item.jsx
+++ frontend/components/lists/list_index_item.jsx
@@ -2,35 +2,90 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 class ListIndexItem extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = this.props.list;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.focusInput = this.focusInput.bind(this);
+    this.blurInput = this.blurInput.bind(this);
+  }
 
-    render() {
-        const { list } = this.props;
+  update(field) {
+    return e => {
+      this.setState({ [field]: e.currentTarget.value });
+    };
+  }
 
-        return (
-            <div className="board-list-container">
-                <div className="list-content-container">
-                    <div className="board-list-title">
-                        <h2>{
-                            list.title
-                        }</h2>
-                    </div>
-                    {/* <div className="cards-container">
+  focusInput(e) {
+      e.preventDefault();
+      debugger
+    //   document.getElementById(`list-title-text-${this.props.list.id}`).classList.add('hidden2');
+      $(`#list-title-text-${this.state.id}`);
+      $(`#list-title-text-${this.state.id}`).addClass("hidden2");
+      $(`#list-title-input-${this.state.id}`).removeClass("hidden2");
+      $(`#list-title-input-${this.state.id}`).focus();
+      $(`#list-title-input-${this.state.id}`).select();
+    // const { list } = this.props;
+    // document.getElementById(`list-title-text-${list.id}`).classList.add('hidden2');
+    // document.getElementById(`list-title-input-${list.id}`).classList.remove('hidden2');
+    // document.getElementById(`list-title-input-${list.id}`).select();
+  }
+  
+  blurInput(e) {
+      e.preventDefault();
+      $(`#list-title-input-${this.state.id}`).blur();
+      $(`#list-title-input-${this.state.id}`).addClass("hidden2");
+      $(`#list-title-text-${this.state.id}`).removeClass("hidden2");
+    // e.preventDefault();
+    // debugger
+    // document.getElementById(`list-title-input-${list.id}`).classList.add("hidden2");
+    // document.getElementById(`list-title-text-${list.id}`).classList.remove("hidden2");
+    // document.getElementById(`list-title-input-${list.id}`).blur();
+  }
+
+  handleSubmit(e) {
+      e.preventDefault();
+      let formData = new FormData();
+      formData.append("list[id]", this.state.id);
+      formData.append("list[title]", this.state.title);
+      this.props.updateList(formData);
+        // let formData = new FormData();
+        // formData.append('list[id]', this.state.id);
+        // formData.append('list[title]', this.state.title);
+        // this.props.updateList(formData);
+  }
+
+  render() {
+    const { list } = this.props;
+
+    return (
+      <div className="board-list-container">
+        <div className="list-content-container">
+          <div className="board-list-title">
+            <div id={`list-title-text-${list.id}`} onClick={this.focusInput} className="list-title-text board-name">
+                {this.state.title}
+            </div>
+            <form onSubmit={this.blurInput}>
+              <input
+                id={`list-title-input-${list.id}`}
+                type="text"
+                value={this.state.title}
+                onChange={this.update("title")}
+                onBlur={this.handleSubmit}
+                className="list-title-input board-name hidden2"
+              />
+            </form>
+          </div>
+          {/* <div className="cards-container">
                         <div>card 1</div>
                         <div>card 2</div>
                         <div>card 3</div>
                         <div>card 4</div>
                     </div> */}
-                </div>
-            </div>
-
-
-
-
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ListIndexItem;
