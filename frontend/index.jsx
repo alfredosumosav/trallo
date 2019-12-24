3575c28ac56ebb9b80e865bea7558583d6d2b29b
alfredosumosav <asumosa1993@gmail.com>
2019-12-23 16:20:32 -0800

    Clean code

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,20 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import {
-        requestBoards,
-        requestBoard,
-        createBoard,
-        updateBoard,
-        deleteBoard
-    } from './actions/board_actions';
-// TESTING
-// import {
-//     signup,
-//     logout,
-//     login
-// } from './actions/session_actions';
-// TESTING END
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
@@ -33,23 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         store = configureStore();
     }
-
-    window.dispatch = store.dispatch;
-    window.getState = store.dispatch;
-    window.requestBoards = requestBoards;
-
-    // now we can test our code from the console
-    // dispatch(fetchBenches()).then(console.log);
-    // TESTING
-    // window.user = {
-    //     username: "alfredo",
-    //     password: "alfredo"
-    // };
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
-    // window.login = login;
-    // window.logout = logout;
-    // TESTING END
 
     ReactDOM.render(<Root store={store} />, root);
 })
