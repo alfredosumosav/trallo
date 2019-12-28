7620d14dc3588510540b0deeda0a8e5067e7c002
alfredosumosav <asumosa1993@gmail.com>
2019-12-27 18:49:00 -0800

    Change trello text to trallo

--- frontend/components/splash_get_started.jsx
+++ frontend/components/splash_get_started.jsx
@@ -6,7 +6,7 @@ const SplashGetStarted = (props) => {
         <div className="get-started-container">
             <div id="gs1" className="splash-column">
                 <h3>Start Planning Today</h3>
-                <p>Sign up and become one of the millions of people around the world using Trello to get more done.</p>
+                <p>Sign up and become one of the millions of people around the world using Trallo to get more done.</p>
                 <p><Link className="btn btn-success" to="/signup">Get&nbsp;Started&nbsp;-&nbsp;It's&nbsp;Free!</Link></p>
             </div>
         </div>
