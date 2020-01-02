24a677af17def3a7bdac46a4f194961ac555cf77
alfredosumosav <asumosa1993@gmail.com>
2020-01-02 11:34:40 -0800

    Refactor unnecessary code

--- frontend/components/cards/card_index_container.js
+++ frontend/components/cards/card_index_container.js
@@ -1,16 +1,16 @@
 import { connect } from 'react-redux';
 import CardIndex from './card_index';
-import { requestCards, updateCard, deleteCard, removeCards } from '../../actions/card_actions';
+import { requestCards } from '../../actions/card_actions';
 
 const mSTP = state => ({
-    lists: Object.values(state.entities.lists)
+
 });
 
 const mDTP = dispatch => ({
-    requestCards: listId => dispatch(requestCards(listId)),
-    updateCard: card => dispatch(updateCard(card)),
-    deleteCard: cardId => dispatch(deleteCard(cardId)),
-    removeCards: () => dispatch(removeCards())
+    // requestCards: data => dispatch(requestCards(data)),
+    // updateCard: card => dispatch(updateCard(card)),
+    // deleteCard: cardId => dispatch(deleteCard(cardId)),
+    // removeCards: () => dispatch(removeCards())
 });
 
 export default connect(mSTP, mDTP)(CardIndex);
