6403e8b4e7994da8e62835abe686e97414eef964
alfredosumosav <asumosa1993@gmail.com>
2020-01-01 17:53:53 -0800

    Create cards migration

---
+++ db/migrate/20200102013729_create_cards.rb
@@ -0,0 +1,19 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.string :title, null: false
+      t.text :description
+      t.datetime :due_date
+      t.integer :board_id, null: false
+      t.integer :list_id, null: false
+      t.integer :author_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :cards, :title
+    add_index :cards, :board_id
+    add_index :cards, :list_id
+    add_index :cards, :author_id
+  end
+end
