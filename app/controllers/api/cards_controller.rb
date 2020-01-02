a165980509e1d95638c7fde20fbab8e16a4604c3
alfredosumosav <asumosa1993@gmail.com>
2020-01-02 11:32:54 -0800

    Fetch cards from current board

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,45 +1,43 @@
 class Api::CardsController < ApplicationController
     def index
-        # debugger
-        # @lists = @lists || List.all
-        # @lists = List.in_board(params[board_id])
-        @cards = Card.all.where(list_id: params[:listId])
+        board = Board.find(params[:boardId])
+        @cards = board.cards
         render :index
     end
 
-    def create
-        @card = Card.new(card_params)
-        @card.author_id = current_user.id
-        if @card.save
-            render :show
-        else
-            render json: @list.errors.full_messages, status: 404
-        end
-    end
+    # def create
+    #     @card = Card.new(card_params)
+    #     @card.author_id = current_user.id
+    #     if @card.save
+    #         render :show
+    #     else
+    #         render json: @card.errors.full_messages, status: 404
+    #     end
+    # end
 
-    def update
-        @card = Card.find_by(id: params[:card][:id])
-        # @card.title = params[:card][:title]
-        # @card.description = params[:card][:description]
-        # @card.due_date = params[:card][:due_date]
-        # @card.list_id = params[:card][:list_id]
+    # def update
+    #     @card = Card.find_by(id: params[:card][:id])
+    #     # @card.title = params[:card][:title]
+    #     # @card.description = params[:card][:description]
+    #     # @card.due_date = params[:card][:due_date]
+    #     # @card.list_id = params[:card][:list_id]
 
-        if @card.update(card_params)
-            render :show
-        else
-            render json: @list.errors.full_messages, status: 404
-        end
-    end
+    #     if @card.update(card_params)
+    #         render :show
+    #     else
+    #         render json: @card.errors.full_messages, status: 404
+    #     end
+    # end
 
-    def destroy
-        @card = Card.find_by(id: params[:id])
+    # def destroy
+    #     @card = Card.find_by(id: params[:id])
 
-        if @card.destroy
-            render :show
-        else
-            render json: { message: 'Could not be deleted' }, status: 404
-        end
-    end
+    #     if @card.destroy
+    #         render :show
+    #     else
+    #         render json: { message: 'Could not be deleted' }, status: 404
+    #     end
+    # end
 
     private
 
