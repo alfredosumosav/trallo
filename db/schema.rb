ea2386be728fc5a13f51dd3a3d1b25cdab679e7d
alfredosumosav <asumosa1993@gmail.com>
2020-01-01 17:54:15 -0800

    Add cards to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_214828) do
+ActiveRecord::Schema.define(version: 2020_01_02_013729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,21 @@ ActiveRecord::Schema.define(version: 2019_12_11_214828) do
     t.boolean "archived", default: false, null: false
     t.index ["author_id"], name: "index_boards_on_author_id"
     t.index ["title"], name: "index_boards_on_title"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.datetime "due_date"
+    t.integer "board_id", null: false
+    t.integer "list_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_cards_on_author_id"
+    t.index ["board_id"], name: "index_cards_on_board_id"
+    t.index ["list_id"], name: "index_cards_on_list_id"
+    t.index ["title"], name: "index_cards_on_title"
   end
 
   create_table "lists", force: :cascade do |t|
