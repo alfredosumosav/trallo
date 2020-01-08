42efa3107125aebd40e68562aa7d7f1b150e3b7a
alfredosumosav <asumosa1993@gmail.com>
2020-01-08 11:09:36 -0800

    Create basic skeleton

---
+++ frontend/components/cards/card_show.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, withRouter } from 'react-router-dom';
+
+class CardShow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return null;
+    }
+}
+
+export default CardShow;
