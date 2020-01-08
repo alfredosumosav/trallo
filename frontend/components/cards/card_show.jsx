a04d03c2949bbdacf5ead6cd1fcdd816139c88e8
alfredosumosav <asumosa1993@gmail.com>
2020-01-08 14:27:22 -0800

    Create card show presentational structure

--- frontend/components/cards/card_show.jsx
+++ frontend/components/cards/card_show.jsx
@@ -9,13 +9,132 @@ class CardShow extends React.Component {
     render() {
         return (
             <div className="card-show-content">
-                <div className="card-header">
+                <a href="#">X</a>
+                <div className="card-show-details">
 
-                </div>
-                <div className="card-main">
+                    <div className="card-header">
+                        <div className="card-header-icon">
 
-                </div>
-                <div className="card-sidebar">
+                        </div>
+                        <div className="card-title">
+                            <div className="card-title-label">
+                                Card title
+                            </div>
+                            <div className="card-title-input hidden2">
+
+                            </div>
+                        </div>
+                        <div className="card-current-list">
+                            <p>In list 
+                                <a href="#">list name</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card-main">
+
+                        <div className="card-main-desc">
+
+                            <div className="card-desc-title">
+                                <div className="card-desc-icon">
+
+                                </div>
+                                <h3>Description</h3>
+                            </div>
+
+                            <div className="card-desc-content">
+
+                                <div className="card-desc-summary-label">
+
+                                </div>
+
+                                <div className="card-desc-input-container">
+
+                                    <div className="card-desc-input">
+
+                                    </div>
+
+                                    <div className="edit controls">
+                                        <div className="desc-save">
+
+                                        </div>
+                                        <div className="desc-cancel">
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="card-activity">
+                            <div className="card-activity-title">
+                                <div className="activity-icon"></div>
+                                <div className="activity-title">
+                                    <h3>Activity</h3>
+                                </div>
+                                <div className="activity-options">
+                                    
+                                </div>
+                            </div>
+
+                            <div className="new-comment-box">
+                                <div className="member-initials"></div>
+                                <div className="comment-form-container focused">
+                                    <div className="form">
+                                        <div className="textarea-input"></div>
+                                        <div className="comment-controls">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="comments-container">
+                                <div className="comment-user"></div>
+                                <div className="comment-info"></div>
+
+                                <div className="comment">
+
+                                </div>
+
+                                <div className="comment-actions"></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="card-sidebar">
+
+                        <div className="add-to-card">
+                            <h3>Add To Card</h3>
+                            <div className="add-to-card-actions">
+                                <div className="due-date">
+                                    <a href="#">Due Date</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="sidebar-actions">
+                            <h3>Actions</h3>
+
+                            <div className="move-card">
+                                <div className="move-icon"></div>
+                                <div><a href="#">Move</a></div>
+                            </div>
+
+                            <div className="copy-card">
+                                <div className="copy-icon"></div>
+                                <div><a href="#">Copy</a></div>
+                            </div>
+
+                            <div className="share-card">
+                                <div className="share-icon"></div>
+                                <div><a href="#">Share</a></div>
+                            </div>
+
+                        </div>
+                    </div>
 
                 </div>
             </div>
