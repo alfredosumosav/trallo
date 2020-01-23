61d7cb9adf0ba5b4309ea59a5284ae76adb70792
alfredosumosav <asumosa1993@gmail.com>
2020-01-23 11:24:35 -0800

    Render board_show_cont and add board/card props

--- frontend/components/cards/card_show_container.jsx
+++ frontend/components/cards/card_show_container.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import CardShow from './card_show';
-import { requestCard, updateCard, deleteCard } from '../../actions/card_actions';
+import { requestCard, updateCard, deleteCard, removeCard } from '../../actions/card_actions';
+import { requestBoard } from '../../actions/board_actions';
+import BoardShowContainer from '../boards/board_show_container';
 
 class CardShowEdit extends React.Component {
 
@@ -10,14 +12,23 @@ class CardShowEdit extends React.Component {
         this.props.requestCard(this.props.match.params.cardId);
     }
 
-    render() {
+    componentDidUpdate(prevProps) {
+        if (prevProps.location.pathname !== this.props.location.pathname) {
+            this.props.removeCard(this.props.card.id);
+            this.props.requestCard(this.props.match.params.cardId).then(() => this.forceUpdate());
+        }
+    }
 
+    render() {
         if (this.props.card === undefined) {
             return null;
         }
         
         return (
-            <CardShow card={this.props.card} updateCard={this.props.updateCard} deleteCard={this.props.deleteCard} />
+            <div>
+                <BoardShowContainer />
+                <CardShow card={this.props.card} updateCard={this.props.updateCard} deleteCard={this.props.deleteCard} />
+            </div>
         )
     }
 }
@@ -25,6 +36,7 @@ class CardShowEdit extends React.Component {
 const mSTP = (state, ownProps) => {
     return {
         card: state.entities.cards[ownProps.match.params.cardId],
+        board: state.entities.boards[ownProps.match.params.boardId]
     }
 };
 
@@ -32,6 +44,8 @@ const mDTP = (dispatch, ownProps) => ({
     requestCard: cardId => dispatch(requestCard(cardId)),
     updateCard: card => dispatch(updateCard(card)),
     deleteCard: cardId => dispatch(deleteCard(cardId)),
+    requestBoard: boardId => dispatch(requestBoard(boardId)),
+    removeCard: cardId => dispatch(removeCard(cardId))
 });
 
 export default withRouter(connect(mSTP, mDTP)(CardShowEdit));
