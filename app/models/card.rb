d7802dd92c719a0d8d86d31ecc9dbfaef45a8fd0
alfredosumosav <asumosa1993@gmail.com>
2020-01-25 13:46:38 -0800

    Add comments association

--- app/models/card.rb
+++ app/models/card.rb
@@ -24,4 +24,10 @@ class Card < ApplicationRecord
         primary_key: :id,
         foreign_key: :author_id,
         class_name: :User
+
+    has_many :comments,
+        primary_key: :id,
+        foreign_key: :card_id,
+        class_name: :Comment,
+        dependent: :destroy
 end
