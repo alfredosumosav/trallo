b08e42e886b9bf9325a17056e1b6911b633854a9
alfredosumosav <asumosa1993@gmail.com>
2020-01-29 13:19:15 -0800

    Create backend update destroy actions

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -19,29 +19,26 @@ class Api::CommentsController < ApplicationController
     #     render :show
     # end
 
-    # def update
-    #     @card = Card.find_by(id: params[:card][:id])
-        # @card.title = params[:card][:title]
-        # @card.description = params[:card][:description]
-        # @card.due_date = params[:card][:due_date]
-        # @card.list_id = params[:card][:list_id]
+    def update
+        @comment = Comment.find_by(id: params[:comment][:id])
+        @comment.body = params[:comment][:body]
 
-    #     if @card.update(card_params)
-    #         render :show
-    #     else
-    #         render json: @card.errors.full_messages, status: 404
-    #     end
-    # end
+        if @comment.update(comment_params)
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: 404
+        end
+    end
 
-    # def destroy
-    #     @card = Card.find_by(id: params[:id])
+    def destroy
+        @comment = Comment.find_by(id: params[:id])
 
-    #     if @card.destroy
-    #         render :show
-    #     else
-    #         render json: { message: 'Could not be deleted' }, status: 404
-    #     end
-    # end
+        if @comment.destroy
+            render :show
+        else
+            render json: { message: 'Could not be deleted' }, status: 404
+        end
+    end
 
     private
 
