a42a64010c0ae4c2317125b8ea8ed1a1e2b6c1d3
alfredosumosav <asumosa1993@gmail.com>
2020-01-30 18:10:12 -0800

    Refactor to use selectors

--- frontend/components/lists/list_index_container.js
+++ frontend/components/lists/list_index_container.js
@@ -1,21 +1,30 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import ListIndex from './list_index';
 import { requestLists, updateList, deleteList, removeLists } from '../../actions/list_actions';
 import { requestCards, createCard, removeCards } from '../../actions/card_actions';
+import { getAllLists, getAllCards } from '../../reducers/selectors';
 
-const mSTP = state => ({
-    lists: Object.values(state.entities.lists),
-    cards: Object.values(state.entities.cards)
-});
+const mSTP = (state, ownProps) => {
+    let boardId = parseInt(ownProps.match.params.boardId);
+    debugger
+    return ({
+        lists: getAllLists(state, boardId),
+        cards: getAllCards(state, boardId)
+    });
+};
 
-const mDTP = dispatch => ({
-    requestLists: boardId => dispatch(requestLists(boardId)),
-    updateList: list => dispatch(updateList(list)),
-    deleteList: listId => dispatch(deleteList(listId)),
-    removeLists: () => dispatch(removeLists()),
-    requestCards: () => dispatch(requestCards()),
-    createCard: card => dispatch(createCard(card)),
-    removeCards: () => dispatch(removeCards())
-});
+const mDTP = dispatch => {
+    debugger
+    return ({
+        requestLists: () => dispatch(requestLists()),
+        updateList: list => dispatch(updateList(list)),
+        deleteList: listId => dispatch(deleteList(listId)),
+        removeLists: () => dispatch(removeLists()),
+        requestCards: () => dispatch(requestCards()),
+        createCard: card => dispatch(createCard(card)),
+        removeCards: () => dispatch(removeCards())
+    });
+};
 
-export default connect(mSTP, mDTP)(ListIndex);
+export default withRouter(connect(mSTP, mDTP)(ListIndex));
