da54989ccd1d8eb9d1291f9f020cce6a5a75173f
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:35:06 -0800

    Make splash_container prettier / remove code

--- frontend/components/splash_container.jsx
+++ frontend/components/splash_container.jsx
@@ -1,13 +1,9 @@
-import Splash from './splash';
-import { connect } from 'react-redux';
-import { signup, removeErrors } from '../actions/session_actions';
-
-const mSTP = state => ({
-
-});
+import Splash from "./splash";
+import { connect } from "react-redux";
+import { signup } from "../actions/session_actions";
 
 const mDTP = dispatch => ({
-    processForm: user => dispatch(signup(user))
+  processForm: user => dispatch(signup(user))
 });
 
-export default connect(mSTP, mDTP)(Splash);
+export default connect(null, mDTP)(Splash);
