78f3ae7c54899085fad1448b459bf7a1648ceb8e
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:16:36 -0800

    Makes splash prettier

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import SplashWelcome from './splash_welcome';
-import SplashCollaborate from './splash-collaborate';
-import SplashFeatures from './splash_features';
-import SplashGetStarted from './splash_get_started';
-import Footer from './footer';
+import React from "react";
+import SplashWelcome from "./splash_welcome";
+import SplashCollaborate from "./splash-collaborate";
+import SplashFeatures from "./splash_features";
+import SplashGetStarted from "./splash_get_started";
+import Footer from "./footer";
 
 class Splash extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="splash">
-                <div className="splash-welcome-container">
-                    <SplashWelcome />
-                </div>
-                <div className="splash-collaborate-container">
-                    <SplashCollaborate />
-                </div>
-                <div className="splash-features-container">
-                    <SplashFeatures />
-                </div>
-                <div id="get-started" className="splash-get-started-container">
-                    <SplashGetStarted />
-                </div>
-                <div id="footer-container1">
-                        <Footer />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="splash">
+        <div className="splash-welcome-container">
+          <SplashWelcome />
+        </div>
+        <div className="splash-collaborate-container">
+          <SplashCollaborate />
+        </div>
+        <div className="splash-features-container">
+          <SplashFeatures />
+        </div>
+        <div id="get-started" className="splash-get-started-container">
+          <SplashGetStarted />
+        </div>
+        <div id="footer-container1">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Splash;
