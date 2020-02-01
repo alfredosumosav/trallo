3ee6e958002bf9b6f37c47786cf2ee62f50384d7
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:39:10 -0800

    Make login_form prettier

--- frontend/components/login_form_container.jsx
+++ frontend/components/login_form_container.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import SessionForm from './session_form';
-import { connect } from 'react-redux';
-import { login, removeErrors } from '../actions/session_actions';
+import SessionForm from "./session_form";
+import { connect } from "react-redux";
+import { login, removeErrors } from "../actions/session_actions";
 
-const mSTP = (state, ownProps) => ({
-    errors: state.errors.session,
-    formType: 'login'
+const mSTP = state => ({
+  errors: state.errors.session,
+  formType: "login"
 });
 
-const mDTP = (dispatch, ownProps) => ({
-    processForm: user => dispatch(login(user)),
-    removeErrors: () => dispatch(removeErrors()),
-    login: user => dispatch(login(user))
+const mDTP = dispatch => ({
+  processForm: user => dispatch(login(user)),
+  removeErrors: () => dispatch(removeErrors()),
+  login: user => dispatch(login(user))
 });
 
 export default connect(mSTP, mDTP)(SessionForm);
