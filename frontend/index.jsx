4d8796c3e782e9430c6a2e576dc0fe43b12b9818
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:40:27 -0800

    Make index component prettier

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
-import Root from './components/root';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    window.fetch = fetch;
-    let store;
-    if (window.currentUser) {
-        const preloadedState = {
-            entities: {
-                users: { [Object.values(currentUser)[0].id]: Object.values(currentUser)[0] }
-            },
-            session: { id: Object.values(currentUser)[0].id }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-    } else {
-        store = configureStore();
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  window.fetch = fetch;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: {
+          [Object.values(currentUser)[0].id]: Object.values(currentUser)[0]
+        }
+      },
+      session: { id: Object.values(currentUser)[0].id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
-    ReactDOM.render(<Root store={store} />, root);
-})
+  ReactDOM.render(<Root store={store} />, root);
+});
