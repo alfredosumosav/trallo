b0a5946bba160b9484e99884fee2f48b36c97238
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:36:44 -0800

    Remove props / Make signup_form_container prettier

--- frontend/components/signup_form_container.jsx
+++ frontend/components/signup_form_container.jsx
@@ -1,15 +1,15 @@
-import SessionForm from './session_form';
-import { connect } from 'react-redux';
-import { signup, removeErrors } from '../actions/session_actions';
+import SessionForm from "./session_form";
+import { connect } from "react-redux";
+import { signup, removeErrors } from "../actions/session_actions";
 
-const mSTP = (state, ownProps) => ({
-    errors: state.errors.session,
-    formType: 'signup'
+const mSTP = state => ({
+  errors: state.errors.session,
+  formType: "signup"
 });
 
-const mDTP = (dispatch, ownProps) => ({
-    processForm: user => dispatch(signup(user)),
-    removeErrors: () => dispatch(removeErrors())
+const mDTP = dispatch => ({
+  processForm: user => dispatch(signup(user)),
+  removeErrors: () => dispatch(removeErrors())
 });
 
 export default connect(mSTP, mDTP)(SessionForm);
