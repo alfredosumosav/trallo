acb707d3a810951eb3b67c996f8faa7ba339d810
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:48:56 -0800

    Make list_index component prettier

--- frontend/components/lists/list_index.jsx
+++ frontend/components/lists/list_index.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Redirect, Route, withRouter } from 'react-router-dom';
-import ListIndexItem from '../lists/list_index_item';
+import React from "react";
+import { Redirect, Route, withRouter } from "react-router-dom";
+import ListIndexItem from "../lists/list_index_item";
 
 class ListIndex extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount() {
-        // debugger
-        this.props.requestLists();
-        this.props.requestCards();
-    };
+  componentDidMount() {
+    this.props.requestLists();
+    this.props.requestCards();
+  }
 
-    render() {
+  render() {
+    const { lists, cards } = this.props;
 
-        const { lists, cards } = this.props;
-        // debugger
-
-        return (
-                    lists.map((list) => <ListIndexItem key={list.id} list={list} cards={cards} updateList={this.props.updateList} deleteList={this.props.deleteList} createCard={this.props.createCard} />)
-        );
-    }
+    return lists.map(list => (
+      <ListIndexItem
+        key={list.id}
+        list={list}
+        cards={cards}
+        updateList={this.props.updateList}
+        deleteList={this.props.deleteList}
+        createCard={this.props.createCard}
+      />
+    ));
+  }
 }
 
 export default ListIndex;
