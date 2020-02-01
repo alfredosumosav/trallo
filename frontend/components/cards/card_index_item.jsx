90130600fa083f63c9dda153223530d3551f26bf
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:54:26 -0800

    Make card_index_item component prettier

--- frontend/components/cards/card_index_item.jsx
+++ frontend/components/cards/card_index_item.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-const CardIndexItem = (props) => {
-    return (
-        <div className="cards-content-container">
-            <div className="list-card-details">
-                <Link to={`/boards/${props.card.board_id}/cards/${props.card.id}`} >
-                    <div className="c-title">
-                        {props.card.title}
-                    </div>
-                </Link>
-            </div>
-        </div>
-    );
-}
+const CardIndexItem = props => {
+  return (
+    <div className="cards-content-container">
+      <div className="list-card-details">
+        <Link to={`/boards/${props.card.board_id}/cards/${props.card.id}`}>
+          <div className="c-title">{props.card.title}</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(CardIndexItem);
