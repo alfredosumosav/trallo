53c42c0cd7d2b10b5f6f9ccc807e920e62fedbfa
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:34:15 -0800

    Make splash-get-starter prettier

--- frontend/components/splash_get_started.jsx
+++ frontend/components/splash_get_started.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const SplashGetStarted = (props) => {
-    return (
-        <div className="get-started-container">
-            <div id="gs1" className="splash-column">
-                <h3>Start Planning Today</h3>
-                <p>Sign up and become one of the millions of people around the world using Trallo to get more done.</p>
-                <p><Link className="btn btn-success" to="/signup">Get&nbsp;Started&nbsp;-&nbsp;It's&nbsp;Free!</Link></p>
-            </div>
-        </div>
-    );
-}
+const SplashGetStarted = props => {
+  return (
+    <div className="get-started-container">
+      <div id="gs1" className="splash-column">
+        <h3>Start Planning Today</h3>
+        <p>
+          Sign up and become one of the millions of people around the world
+          using Trallo to get more done.
+        </p>
+        <p>
+          <Link className="btn btn-success" to="/signup">
+            Get&nbsp;Started&nbsp;-&nbsp;It's&nbsp;Free!
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default SplashGetStarted;
