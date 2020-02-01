94dd4258aa012b5e6fa5ac18eaf55775754e6566
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:49:09 -0800

    Make list_index_item component prettier

--- frontend/components/lists/list_index_item.jsx
+++ frontend/components/lists/list_index_item.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import CardIndex from '../cards/card_index';
+import React from "react";
+import CardIndex from "../cards/card_index";
 
 class ListIndexItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.props.list;
-    this.state.card_title = '';
+    this.state.card_title = "";
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.focusInput = this.focusInput.bind(this);
@@ -20,52 +20,62 @@ class ListIndexItem extends React.Component {
   }
 
   focusInput(e) {
-      e.preventDefault();
-      $(`#list-title-text-${this.state.id}`);
-      $(`#list-title-text-${this.state.id}`).addClass("hidden2");
-      $(`#list-title-input-${this.state.id}`).removeClass("hidden2");
-      $(`#list-title-input-${this.state.id}`).focus();
-      $(`#list-title-input-${this.state.id}`).select();
+    e.preventDefault();
+    $(`#list-title-text-${this.state.id}`);
+    $(`#list-title-text-${this.state.id}`).addClass("hidden2");
+    $(`#list-title-input-${this.state.id}`).removeClass("hidden2");
+    $(`#list-title-input-${this.state.id}`).focus();
+    $(`#list-title-input-${this.state.id}`).select();
   }
-  
+
   blurInput(e) {
-      e.preventDefault();
-      $(`#list-title-input-${this.state.id}`).blur();
+    e.preventDefault();
+    $(`#list-title-input-${this.state.id}`).blur();
   }
 
   handleSubmit(e) {
-      e.preventDefault();
-      $(`#list-title-input-${this.state.id}`).addClass("hidden2");
-      $(`#list-title-text-${this.state.id}`).removeClass("hidden2");
-      let formData = new FormData();
-      formData.append("list[id]", this.state.id);
-      formData.append("list[title]", this.state.title);
-      this.props.updateList(formData);
+    e.preventDefault();
+    $(`#list-title-input-${this.state.id}`).addClass("hidden2");
+    $(`#list-title-text-${this.state.id}`).removeClass("hidden2");
+    let formData = new FormData();
+    formData.append("list[id]", this.state.id);
+    formData.append("list[title]", this.state.title);
+    this.props.updateList(formData);
   }
 
   handleNewCard(e) {
     e.preventDefault();
 
     let cardFormData = new FormData();
-    cardFormData.append('card[title]', this.state.card_title);
-    cardFormData.append('card[list_id]', this.state.id);
+    cardFormData.append("card[title]", this.state.card_title);
+    cardFormData.append("card[list_id]", this.state.id);
 
-    this.props.createCard(cardFormData).then(e => {
-
-      this.setState({
-        card_title: ''
-      }, (e) => {
-        document.getElementById(`card-input-${this.state.id}`).focus();
-        // scroll to form for easier exp
-        document.getElementById(`card-f-${this.state.id}`).scrollIntoView(false);
+    this.props
+      .createCard(cardFormData)
+      .then(e => {
+        this.setState(
+          {
+            card_title: ""
+          },
+          e => {
+            document.getElementById(`card-input-${this.state.id}`).focus();
+            // scroll to form for easier exp
+            document
+              .getElementById(`card-f-${this.state.id}`)
+              .scrollIntoView(false);
+            return null;
+          }
+        );
+      })
+      .fail(e => {
+        document
+          .getElementById(`card-f-${this.state.id}`)
+          .classList.add("hidden2");
+        document
+          .getElementById(`card-text-${this.state.id}`)
+          .classList.remove("hidden2");
         return null;
       });
-
-    }).fail(e => {
-      document.getElementById(`card-f-${this.state.id}`).classList.add('hidden2');
-      document.getElementById(`card-text-${this.state.id}`).classList.remove('hidden2');
-      return null;
-    });
   }
 
   handleDelete(e) {
@@ -79,13 +89,26 @@ class ListIndexItem extends React.Component {
     return (
       <div className="board-list-container">
         <div className="list-content-container">
-          <div onMouseEnter={() => $(`#delete-${this.state.id}`).removeClass('hidden2')} onMouseLeave={() => $(`#delete-${this.state.id}`).addClass('hidden2')} className="board-list-title">
-              <div id={`list-title-text-${list.id}`} className="list-title-text board-name">
-                <div onClick={this.focusInput}>
-                  {this.state.title}
-                </div>
-                <i id={`delete-${this.state.id}`} onClick={this.handleDelete} className="far fa-trash-alt btn hidden2"></i>
-              </div>
+          <div
+            onMouseEnter={() =>
+              $(`#delete-${this.state.id}`).removeClass("hidden2")
+            }
+            onMouseLeave={() =>
+              $(`#delete-${this.state.id}`).addClass("hidden2")
+            }
+            className="board-list-title"
+          >
+            <div
+              id={`list-title-text-${list.id}`}
+              className="list-title-text board-name"
+            >
+              <div onClick={this.focusInput}>{this.state.title}</div>
+              <i
+                id={`delete-${this.state.id}`}
+                onClick={this.handleDelete}
+                className="far fa-trash-alt btn hidden2"
+              ></i>
+            </div>
             <form onSubmit={this.blurInput}>
               <input
                 id={`list-title-input-${list.id}`}
@@ -101,75 +124,98 @@ class ListIndexItem extends React.Component {
           <div className="list-cards-container">
             <CardIndex list={list} cards={cards} />
 
-              <div 
-                id={`card-text-${this.state.id}`}
-                onClick={e => {
-                  document.getElementById(`card-text-${this.state.id}`).classList.add('hidden2');
-                  document.getElementById(`card-f-${this.state.id}`).classList.remove('hidden2');
-                  document.getElementById(`card-input-${this.state.id}`).focus();
-                }}
-                className="list-card-details cards-content-container card-text card-input-container"
-              >
-                  <i className="fas fa-plus"></i> Add a card
-              </div>
-            
-            <div className="c-form-container">
+            <div
+              id={`card-text-${this.state.id}`}
+              onClick={e => {
+                document
+                  .getElementById(`card-text-${this.state.id}`)
+                  .classList.add("hidden2");
+                document
+                  .getElementById(`card-f-${this.state.id}`)
+                  .classList.remove("hidden2");
+                document.getElementById(`card-input-${this.state.id}`).focus();
+              }}
+              className="list-card-details cards-content-container card-text card-input-container"
+            >
+              <i className="fas fa-plus"></i> Add a card
+            </div>
 
+            <div className="c-form-container">
               <form
                 id={`card-f-${this.state.id}`}
                 onSubmit={e => this.handleNewCard(e)}
                 className="card-form-container hidden2"
+              >
+                <input
+                  id={`card-input-${this.state.id}`}
+                  type="text"
+                  value={this.state.card_title}
+                  autoComplete="off"
+                  placeholder="Enter card title..."
+                  onChange={this.update("card_title")}
+                  onBlur={e => {
+                    if (
+                      e.relatedTarget !== null &&
+                      e.relatedTarget.value === "X"
+                    ) {
+                      document
+                        .getElementById(`card-f-${this.state.id}`)
+                        .classList.add("hidden2");
+                      document
+                        .getElementById(`card-text-${this.state.id}`)
+                        .classList.remove("hidden2");
+                      this.setState(
+                        {
+                          card_title: ""
+                        },
+                        e => null
+                      );
+                    } else if (
+                      e.relatedTarget === null ||
+                      e.relatedTarget ||
+                      e.relatedTarget.value !== "X"
+                    ) {
+                      return this.handleNewCard(e);
+                    }
+
+                    return null;
+                  }}
+                  className="card-input"
+                />
+
+                <div
+                  id={`card-actions-container-${this.state.id}`}
+                  className="card-actions"
                 >
-                  <input 
-                    id={`card-input-${this.state.id}`}
-                    type="text"
-                    value={this.state.card_title}
-                    autoComplete="off"
-                    placeholder="Enter card title..."
-                    onChange={this.update("card_title")}
-                    onBlur={e => {
-
-                      if ((e.relatedTarget !== null) && (e.relatedTarget.value === 'X')) {
-                        document.getElementById(`card-f-${this.state.id}`).classList.add('hidden2');
-                        document.getElementById(`card-text-${this.state.id}`).classList.remove('hidden2');
-                        this.setState({
-                          card_title: ''
-                        }, e => null);
-                      } else if (e.relatedTarget === null || e.relatedTarget || e.relatedTarget.value !== 'X') {
-                        return this.handleNewCard(e);
-                      }
-
-                      return null;
-                    }}
-                    className="card-input"
-                  />
-
-                  <div id={`card-actions-container-${this.state.id}`} className="card-actions">
-
-                    <div 
-                      id={`submit-card-input-${this.state.id}`} 
-                      className="btn-success btn-save"
-                      >
-                        Add Card
-                    </div>
-
-                    <input
-                      type="button"
-                      value="X"
-                      onClick={(e) => {
-                        this.setState({
-                          card_title: ''
-                        }, (e) => {
-                          document.getElementById(`card-f-${this.state.id}`).classList.add('hidden2');
-                          document.getElementById(`card-text-${this.state.id}`).classList.remove('hidden2');
-                        });
-                      }}
-                      className="btn-cancel"
-                    />
-
+                  <div
+                    id={`submit-card-input-${this.state.id}`}
+                    className="btn-success btn-save"
+                  >
+                    Add Card
                   </div>
-              </form>
 
+                  <input
+                    type="button"
+                    value="X"
+                    onClick={e => {
+                      this.setState(
+                        {
+                          card_title: ""
+                        },
+                        e => {
+                          document
+                            .getElementById(`card-f-${this.state.id}`)
+                            .classList.add("hidden2");
+                          document
+                            .getElementById(`card-text-${this.state.id}`)
+                            .classList.remove("hidden2");
+                        }
+                      );
+                    }}
+                    className="btn-cancel"
+                  />
+                </div>
+              </form>
             </div>
           </div>
         </div>
