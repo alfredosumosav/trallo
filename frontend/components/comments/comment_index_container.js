ce8e0cc195eccfd27d033ff4b8fa7ebe1b29d646
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:50:06 -0800

    Make comment_index container prettier

--- frontend/components/comments/comment_index_container.js
+++ frontend/components/comments/comment_index_container.js
@@ -1,17 +1,21 @@
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import CommentIndex from './comment_index';
-import { requestComments, updateComment, deleteComment } from '../../actions/comment_actions';
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import CommentIndex from "./comment_index";
+import {
+  requestComments,
+  updateComment,
+  deleteComment
+} from "../../actions/comment_actions";
 
 const mSTP = (state, ownProps) => ({
-    card: state.entities.cards[ownProps.match.params.cardId],
-    comments: Object.values(state.entities.comments)
+  card: state.entities.cards[ownProps.match.params.cardId],
+  comments: Object.values(state.entities.comments)
 });
 
 const mDTP = dispatch => ({
-    requestComments: () => dispatch(requestComments()),
-    updateComment: comment => dispatch(updateComment(comment)),
-    deleteComment: commentId => dispatch(deleteComment(commentId))
+  requestComments: () => dispatch(requestComments()),
+  updateComment: comment => dispatch(updateComment(comment)),
+  deleteComment: commentId => dispatch(deleteComment(commentId))
 });
 
 export default withRouter(connect(mSTP, mDTP)(CommentIndex));
