ea460e1cfeff6cf9e44c550773f3200b31adf905
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:54:16 -0800

    Make card_index component prettier

--- frontend/components/cards/card_index.jsx
+++ frontend/components/cards/card_index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import CardIndexItem from './card_index_item';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import CardIndexItem from "./card_index_item";
 
-const CardIndex = (props) => {
-    if (props.cards.length === 0 || props.list.length === 0) {
-        return null;
-    }
+const CardIndex = props => {
+  if (props.cards.length === 0 || props.list.length === 0) {
+    return null;
+  }
 
-    return (
-        props.cards.filter(card => card.list_id === props.list.id).map((card, id) => <CardIndexItem key={id} card={card} />)
-    );
-}
+  return props.cards
+    .filter(card => card.list_id === props.list.id)
+    .map((card, id) => <CardIndexItem key={id} card={card} />);
+};
 
 export default withRouter(CardIndex);
