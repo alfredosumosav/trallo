7f11adc839b1b88a5bed3fbf284a63bf29d5d158
alfredosumosav <asumosa1993@gmail.com>
2020-02-01 01:48:43 -0800

    Make modal component prettier

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import { closeModal } from '../../actions/modal_actions';
-import { connect } from 'react-redux';
-import CreateBoardFormContainer from '../../components/boards/create_board_form_container';
-import CardShowContainer  from '../cards/card_show_container';
-import CardShow  from '../cards/card_show';
+import React from "react";
+import { closeModal } from "../../actions/modal_actions";
+import { connect } from "react-redux";
+import CreateBoardFormContainer from "../../components/boards/create_board_form_container";
+import CardShowContainer from "../cards/card_show_container";
+import CardShow from "../cards/card_show";
 
 function Modal({ modal, closeModal }) {
-    if (!modal) {
-        return null;
-    }
-    let component;
-    switch (modal) {
-        case 'create_board':
-            component = <CreateBoardFormContainer />;
-            break;
-        case 'showcard':
-            component = <CardShowContainer />;
-            break;
-        default:
-            return null;
-    }
-    return (
-        <div className="modal-background" onClick={closeModal}>
-            <div className="modal-child" onClick={e => e.stopPropagation()}>
-                {component}
-            </div>
-        </div>
-    );
+  if (!modal) {
+    return null;
+  }
+  let component;
+  switch (modal) {
+    case "create_board":
+      component = <CreateBoardFormContainer />;
+      break;
+    case "showcard":
+      component = <CardShowContainer />;
+      break;
+    default:
+      return null;
+  }
+  return (
+    <div className="modal-background" onClick={closeModal}>
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        {component}
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
-    return {
-        modal: state.ui.modal
-    };
+  return {
+    modal: state.ui.modal
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        closeModal: () => dispatch(closeModal())
-    };
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Modal);
